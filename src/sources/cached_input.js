794043d5401dcfb2994fca286019468a4f3a1a01
Douglas A. Seifert <doug@dseifert.net>
2016-08-11 07:27:07 -0700

    fix: handle zero length cached files
    Add test for cached input.

--- src/sources/cached_input.js
+++ src/sources/cached_input.js
@@ -13,14 +13,14 @@ export class CachedInput extends Readable {
       let lastMod = new Date(stats.mtime);
       let now = new Date();
       let maxAge = options.maxAge || 86400000;
-      if (lastMod.getTime() + maxAge > now.getTime()) {
+      if (stats.size > 0 && lastMod.getTime() + maxAge > now.getTime()) {
         config.log.debug(`Using cached data at ${this.cacheFile}`);
         this.inputCreate = () => {
           return fs.createReadStream(this.cacheFile);
         }
       } else {
         this.output = fs.createWriteStream(this.cacheFile);
-        config.log.debug(`Cached data has expired, regenerating ${this.cacheFile}`);
+        config.log.debug(`Cached data has expired or is empty, regenerating ${this.cacheFile}`);
       }
     } else {
       this.output = fs.createWriteStream(this.cacheFile);
---
+++ test/src/cached_source_test.js
@@ -0,0 +1,120 @@
+const {expect} = require('chai');
+import {config} from '../../src/config';
+import {CsvSource} from '../../src/sources/csv_source';
+import fs from 'fs';
+
+describe('cached source', function() {
+  before( () => {
+    config.addAppSpecific();
+    this.urlFormatter = config.defaultUrlFormatter({urlBase: "http://test.com"});
+    this.cacheFile = `${process.cwd()}/tmp/cachefile.txt`;
+  });
+  after( () => {
+    fs.unlinkSync(this.cacheFile);
+  });
+  it('saves a cache file', (done) => {
+    let urls = [];
+    let source = new CsvSource({siteMap: {changefreq: 'foo', priority: 1, channel: 'csv',
+      urlFormatter: this.urlFormatter}, input: {cached: {cacheFile: this.cacheFile, maxAge: 86000000}, fileName: `${process.cwd()}/test/config/test.csv`}}).
+      on('end', (args) => {
+        expect(urls.length).to.equal(5);
+        expect(urls[0].url).to.equal('http://test.com/url1');
+        expect(urls[1].url).to.equal('http://test.com/url2');
+        expect(urls[1].imageUrl).to.equal('/image2');
+        expect(fs.existsSync(this.cacheFile));
+        done();
+      }).on('error', (err) => {
+        console.log("ERROR: ", err);
+        console.log(err.stack);
+      }).on('data', (url) => {
+        urls.push(url);
+      });
+    source.open();
+  });
+  describe('with existing cached data', function() {
+    before( () => {
+      config.addAppSpecific();
+      this.urlFormatter = config.defaultUrlFormatter({urlBase: "http://test.com"});
+      this.cacheFile = `${process.cwd()}/tmp/cachefile.txt`;
+      fs.writeFileSync(this.cacheFile, fs.readFileSync(`${process.cwd()}/test/config/preexisting_cache.txt`));
+    });
+    it('reads cached data', (done) => {
+      let urls = [];
+      let source = new CsvSource({siteMap: {changefreq: 'foo', priority: 1, channel: 'csv',
+        urlFormatter: this.urlFormatter}, input: {cached: {cacheFile: this.cacheFile, maxAge: 86000000}, fileName: `${process.cwd()}/test/config/test.csv`}}).
+        on('end', (args) => {
+          expect(urls.length).to.equal(5);
+          expect(urls[0].url).to.equal('http://test.com/url1_cached');
+          expect(urls[1].url).to.equal('http://test.com/url2_cached');
+          expect(urls[1].imageUrl).to.equal('/image2_cached');
+          expect(fs.existsSync(this.cacheFile));
+          expect(fs.statSync(this.cacheFile).size).to.be.above(0);
+          done();
+        }).on('error', (err) => {
+          console.log("ERROR: ", err);
+          console.log(err.stack);
+        }).on('data', (url) => {
+          urls.push(url);
+        });
+      source.open();
+    });
+  });
+  describe('with existing zero length cache', function() {
+    before( () => {
+      config.addAppSpecific();
+      this.urlFormatter = config.defaultUrlFormatter({urlBase: "http://test.com"});
+      this.cacheFile = `${process.cwd()}/tmp/cachefile.txt`;
+      fs.writeFileSync(this.cacheFile, "");
+    });
+    it('reads uncached data', (done) => {
+      let urls = [];
+      let source = new CsvSource({siteMap: {changefreq: 'foo', priority: 1, channel: 'csv',
+        urlFormatter: this.urlFormatter}, input: {cached: {cacheFile: this.cacheFile, maxAge: 86000000}, fileName: `${process.cwd()}/test/config/test.csv`}}).
+        on('end', (args) => {
+          expect(urls.length).to.equal(5);
+          expect(urls[0].url).to.equal('http://test.com/url1');
+          expect(urls[1].url).to.equal('http://test.com/url2');
+          expect(urls[1].imageUrl).to.equal('/image2');
+          expect(fs.existsSync(this.cacheFile));
+          expect(fs.statSync(this.cacheFile).size).to.be.above(0);
+          done();
+        }).on('error', (err) => {
+          console.log("ERROR: ", err);
+          console.log(err.stack);
+        }).on('data', (url) => {
+          urls.push(url);
+        });
+      source.open();
+    });
+  });
+  describe('with existing expired cache data', function() {
+    before( () => {
+      config.addAppSpecific();
+      this.urlFormatter = config.defaultUrlFormatter({urlBase: "http://test.com"});
+      this.cacheFile = `${process.cwd()}/tmp/cachefile.txt`;
+      fs.writeFileSync(this.cacheFile, fs.readFileSync(`${process.cwd()}/test/config/preexisting_cache.txt`));
+      let d = new Date(Date.now() - 86400000);
+      fs.utimesSync(this.cacheFile, d, d);
+    });
+    it('reads uncached data', (done) => {
+      let urls = [];
+      let source = new CsvSource({siteMap: {changefreq: 'foo', priority: 1, channel: 'csv',
+        urlFormatter: this.urlFormatter}, input: {cached: {cacheFile: this.cacheFile, maxAge: 86000000}, fileName: `${process.cwd()}/test/config/test.csv`}}).
+        on('end', (args) => {
+          expect(urls.length).to.equal(5);
+          expect(urls[0].url).to.equal('http://test.com/url1');
+          expect(urls[1].url).to.equal('http://test.com/url2');
+          expect(urls[1].imageUrl).to.equal('/image2');
+          expect(fs.existsSync(this.cacheFile));
+          expect(fs.statSync(this.cacheFile).size).to.be.above(0);
+          done();
+        }).on('error', (err) => {
+          console.log("ERROR: ", err);
+          console.log(err.stack);
+        }).on('data', (url) => {
+          urls.push(url);
+        });
+      source.open();
+    });
+  });
+});
