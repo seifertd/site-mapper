05c4b1266df00bfe88b8976a00e59a72cdc0708d
Doug Seifert <dseifert@groupon.com>
2016-09-28 16:14:04 -0700

    fix: Fix ability to handle json source with multiple http input

--- src/sources/multiple_http_input.js
+++ src/sources/multiple_http_input.js
@@ -9,6 +9,11 @@ export class MultipleHttpInput extends Readable {
     this.urls = options.urls.slice();
     this.error = false;
     this.httpOptions = options.httpOptions;
+    this.format = options.format;
+    this.stop = options.stop;
+    this.state = {
+      firstRequest: true
+    };
     if (!this.urls || this.urls.length == 0) {
       throw new Error("No urls specified for MultipleHttpInput: ", this.urls);
     }
@@ -27,17 +32,29 @@ export class MultipleHttpInput extends Readable {
           this.emit('error', new Error(`Non 2XX response from ${url}: ${response.statusCode}`));
         }
       }).on('data', (data) => {
+        if (this.state.firstRequest) {
+          if (this.format == 'json') {
+            this.push("[");
+          }
+          this.state.firstRequest = false;
+        }
         let paused = !this.push(data);
         if (paused) {
           config.log.warn("We should pause, but are not");
         }
       }).on('end', () => {
+        if (this.urls.length > 0 && this.format == 'json') {
+          this.push(",");
+        }
         this._startNextRequest();
       }).on('error', (err) => {
         this.error = true;
         this.emit('error', err);
       });
     } else {
+      if (this.format == 'json') {
+        this.push("]");
+      }
       this.push(null);
     }
   }
